896a992fc680b5e6f287e4ea7f2b961036ba5134
RafaRuizXD <136024665+RafaRuizXD@users.noreply.github.com>
2023-08-31 16:29:57 -0500

    Add files via upload
    xd

---
+++ Login-y-registro-con-html-css-js-main/php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+
+   $conexion = mysqli_connect("localhost", "root", "", "rani2");
+
+   /*if($conexion){
+    echo 'Conectado exitosamente a la base de datos';
+   }else{
+    echo 'No se ah podido conectar a la base de datos';
+   }/*¿¿*/
+
+
+?>
